ccd2069aeba15002be92fa437da20216ad2c1050
YanCh <2291900019@qq.com>
2020-12-29 16:24:48 +0800

    fix:修改文章新增bug|去除SwaggerConfig无效引用
    (cherry picked from commit fe25a0f18e2fcd2b5bc52d5e4f771688aa1e1007)

--- src/main/java/com/github/niefy/config/SwaggerConfig.java
+++ src/main/java/com/github/niefy/config/SwaggerConfig.java
@@ -8,14 +8,9 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2

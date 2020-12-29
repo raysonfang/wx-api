433bd11722d13790ac03551a8466946d1fc7c219
YanCh <2291900019@qq.com>
2020-12-29 16:24:48 +0800

    fix:修改文章新增bug|去除SwaggerConfig无效引用
    (cherry picked from commit 7edf1959ae5c3b9c696861754e1150ab568d7f04)

--- src/main/java/com/github/niefy/config/SwaggerConfig.java
+++ src/main/java/com/github/niefy/config/SwaggerConfig.java
@@ -7,18 +7,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
             .apiInfo(apiInfo())
             .select()
             //加了ApiOperation注解的类，才生成接口文档
